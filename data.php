cc3cba62dbef88de44c551699293a0f53d371006
Roys wanimbo <157364709+royswanimbo@users.noreply.github.com>
2024-09-30 20:13:31 +0900

    commit file baru index.php

---
+++ data.php
@@ -0,0 +1,38 @@
+<?php 
+
+
+$mahasiswa = [
+
+    [
+        'id' => 1,
+        'nim' => "16411045",
+        'nama' => "Roys Wanimbo",
+        'alamat' => "Sentani",
+        'jurusan' => "Teknik Informatika"
+    ],
+
+    [
+        'id' => 2,
+        'nim' => "16411046",
+        'nama' => "Paulus Sesa",
+        'alamat' => "Waena",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 3,
+        'nim' => "16411021",
+        'nama' => "Korinus Wenda",
+        'alamat' => "Waena",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 4,
+        'nim' => "16411121",
+        'nama' => "Billy",
+        'alamat' => "Kotaraja",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+];
